f46b37b9f020be7fb800d69eef44105928ae8aed
VM <mo.sunday@proton.me>
2025-11-23 01:51:53 -0600

    added dark mode everywhere in plasma.nix

--- home/modules/desktop/plasma.nix
+++ home/modules/desktop/plasma.nix
@@ -1,19 +1,52 @@
 { config, pkgs, ... }:
 
 {
-  # Force KDE to use Breeze Dark global theme + colors
+  # Ensure KDE apps honor the dark color scheme
   xdg.configFile."kdeglobals".text = ''
-[KDE]
-LookAndFeelPackage=org.kde.breezedark.desktop
-
 [General]
 ColorScheme=BreezeDark
-  '';
 
-  # Force Plasma style (panel, widgets) to Breeze Dark
+[Icons]
+Theme=breeze-dark
+
+[KDE]
+widgetStyle=Breeze
+LookAndFeelPackage=BreezeDark
+'';  
+
+  # Plasma desktop theme
   xdg.configFile."plasmarc".text = ''
 [Theme]
-name=breeze-dark
-  '';
+name=BreezeDark
+color=BreezeDark
+'';  
 
+  # Window decorations (KWin)
+  xdg.configFile."kwinrc".text = ''
+[org.kde.kdecoration2]
+theme=Breeze
+plugin=org.kde.breeze
+'';  
+
+  # GTK 3 + 4 Themes (fixes Dolphin + other GTK apps)
+  xdg.configFile."gtk-3.0/settings.ini".text = ''
+[Settings]
+gtk-theme-name=Breeze-Dark
+gtk-icon-theme-name=breeze-dark
+gtk-application-prefer-dark-theme=1
+'';  
+
+  xdg.configFile."gtk-4.0/settings.ini".text = ''
+[Settings]
+gtk-theme-name=Breeze-Dark
+gtk-icon-theme-name=breeze-dark
+gtk-application-prefer-dark-theme=1
+'';  
+
+  # Install theme packages (NixOS requires these!)
+  home.packages = with pkgs; [
+    kdePackages.breeze
+    kdePackages.breeze-gtk
+    kdePackages.breeze-icons
+  ];
 }
