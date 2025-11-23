2bbbd24a14dff1c7cb91cbb8b69347a9efc7fc24
VM <mo.sunday@proton.me>
2025-11-23 01:43:53 -0600

    added plasma style and changed the style to dark in kde hm

--- home/modules/desktop/plasma.nix
+++ home/modules/desktop/plasma.nix
@@ -9,4 +9,11 @@ LookAndFeelPackage=org.kde.breezedark.desktop
 [General]
 ColorScheme=BreezeDark
   '';
+
+  # Force Plasma style (panel, widgets) to Breeze Dark
+  xdg.configFile."plasmarc".text = ''
+[Theme]
+name=breeze-dark
+  '';
+
 }
